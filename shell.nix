e2a0b4cdb0a22e2c35c69ac930bf151c05e0b8b3
Dmitry Zhevlov <d.zhevlov@tawasal.ae>
2022-06-21 10:52:43 +0300

    Fix syntax in shell.nix

--- shell.nix
+++ shell.nix
@@ -3,5 +3,5 @@
 mkShell {
   name = "postgresql-jit";
 
-  buildInputs = [ postgresql_10_jit, postgresql_11_jit, postgresql_12_jit, postgresql_13_jit, postgresql_14_jit ];
+  buildInputs = [ postgresql_10_jit postgresql_11_jit postgresql_12_jit postgresql_13_jit postgresql_14_jit ];
 }
